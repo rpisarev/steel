c9886f32a7f5dec09bdce1617034d0de3786fe08
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-03-01 10:17:44 +0200

    Change setup.sh

--- steel/setup.sh
+++ steel/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 init/init_json.py
-if [ "`ls init/initial_data.json`"  ]; then
+if [ "`ls init| grep initial_data.json`"  ]; then
     mv init/initial_data.json ./
     ./init_models.py
 fi
